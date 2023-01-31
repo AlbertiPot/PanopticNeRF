f94b7837418e67c4117ac0843b618fcfd3dfd451
AlbertiPot <1071704933@qq.com>
2023-01-31 10:29:13 +0800

    add final changes

--- launch.sh
+++ launch.sh
@@ -1,17 +1,18 @@
+PYHOME=/root/miniconda3/envs/pnf/bin
 # # # train
-# python train_net.py \
-# --cfg_file ./configs/panopticnerf_test.yaml \
-# gpus [1] \
-# pretrain nerf \
-# use_stereo True \
-# use_pspnet True \
-# use_depth True \
-# pseudo_filter True \
-# weight_th 0.05 \
-# resume False
+${PYHOME}/python train_net.py \
+--cfg_file ./configs/panopticnerf_test.yaml \
+gpus [0] \
+pretrain nerf \
+use_stereo True \
+use_pspnet True \
+use_depth True \
+pseudo_filter True \
+weight_th 0.05 \
+resume False
 
 # Render semantic map, panoptic map and depth map in a single forward pass
-python run.py \
---type visualize \
---cfg_file configs/panopticnerf_test.yaml \
-use_stereo False
+# python run.py \
+# --type visualize \
+# --cfg_file configs/panopticnerf_test.yaml \
+# use_stereo False
